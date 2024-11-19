e841523f52ed10c2b0f523f51443fd718c9882d7
J0kerModZ <167721907+J0kerModZ@users.noreply.github.com>
2024-11-18 22:24:43 -0500

    Lego Build

---
+++ Cheat/Menu.cs
@@ -0,0 +1,53 @@
+﻿using GorillaNetworking;
+using J0kersGuardianMenu.Cheat;
+using Photon.Pun;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace J0kersGuardianMenu
+{
+    public class Menu : MonoBehaviour
+    {
+        static bool Spam;
+        static bool spamOthers;
+
+        public void OnGUI()
+        {
+            GUI.backgroundColor = Color.Lerp(Color.magenta, Color.blue, Mathf.PingPong(Time.time, 1f));
+
+            GUILayout.BeginArea(new Rect(10, 10, 200, 300), GUI.skin.box);
+
+            GUIStyle titleStyle = new GUIStyle(GUI.skin.label);
+            titleStyle.fontSize = 20;
+            titleStyle.alignment = TextAnchor.UpperCenter;
+
+            GUILayout.Label("J0kers Lego Panel", titleStyle);
+
+            if (GUILayout.Button("Block Spammer [BIG]", GUILayout.Height(40)))
+            {
+                Spam = !Spam;
+            }
+            if (Spam)
+            {
+                Mods.BlockSpammer();
+            }
+
+            if (GUILayout.Button("Block Spammer Others [BIG]", GUILayout.Height(40)))
+            {
+                spamOthers = !spamOthers;
+            }
+            if (spamOthers)
+            {
+                Mods.BlockSpammerOther();
+            }
+
+            if (GUILayout.Button("Leave", GUILayout.Height(40)))
+            {
+                PhotonNetwork.Disconnect();
+            }
+            GUILayout.EndArea();
+        }
+    }
+}
---
+++ Cheat/Mods.cs
@@ -0,0 +1,72 @@
+﻿using GorillaTagScripts;
+using Photon.Pun;
+using System.Collections;
+using UnityEngine;
+
+namespace J0kersGuardianMenu.Cheat
+{
+    public class Mods : MonoBehaviour
+    {
+        private static bool LostFrame;
+        static float SpamTime = 0f;
+        private static float rpcBypassCooldown = 5f;
+        private static float lastRpcBypassTime;
+
+
+        public static void RPCBypass()
+        {
+            if (Time.time - lastRpcBypassTime < rpcBypassCooldown)
+                return;
+
+            lastRpcBypassTime = Time.time;
+
+            try
+            {
+                if (!LostFrame)
+                {
+                    LostFrame = true;
+                    PhotonNetwork.OpCleanActorRpcBuffer(PhotonNetwork.LocalPlayer.ActorNumber);
+
+                    GorillaNot.instance.logErrorMax = 1000;
+                    GorillaNot.instance.rpcErrorMax = 1000;
+                    GorillaNot.instance.rpcCallLimit = 1000;
+
+                    PhotonNetwork.MaxResendsBeforeDisconnect = 5;
+                    PhotonNetwork.QuickResends = 2;
+
+                    GorillaNot.instance.OnPlayerLeftRoom(PhotonNetwork.LocalPlayer);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError($"RPCBypass Error: {ex.Message}");
+            }
+        }
+
+        public static void BlockSpammer()
+        {
+            if (Time.time > SpamTime)
+            {
+                BuilderTable.instance.RequestCreatePiece(-566818631, GorillaTagger.Instance.offlineVRRig.transform.position, GorillaTagger.Instance.offlineVRRig.transform.rotation, -1);
+                RPCBypass();
+                SpamTime = Time.time + 0.1f;
+            }
+        }
+
+        public static void BlockSpammerOther()
+        {
+            foreach (VRRig rig in GorillaParent.instance.vrrigs)
+            {
+                if (rig != GorillaTagger.Instance.offlineVRRig)
+                {
+                    if (Time.time > SpamTime)
+                    {
+                        BuilderTable.instance.RequestCreatePiece(-566818631, rig.transform.position, rig.transform.rotation, -1);
+                        RPCBypass();
+                        SpamTime = Time.time + 0.1f;
+                    }
+                }
+            }
+        }
+    }
+}
